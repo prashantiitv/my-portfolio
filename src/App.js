9c5829d5e0ffa4694dbbd2199a8dd3fea9ed55ee
Prashant Bharti <prashantiitv@gmail.com>
2020-11-06 02:20:51 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Pages from "./pages";
-//import './App.css';
 
 function App() {
   const [user, setUser] = useState(null);
   useEffect(() => {
-    // NOTE: Use your username below
     fetch("https://gitconnected.com/v1/portfolio/prashantiitv")
       .then((res) => res.json())
       .then((user) => {
