2badce25cf30ca7ab827a2e7d01131ed99145c89
Prashant Bharti <prashantiitv@gmail.com>
2023-04-17 03:08:48 +0530

    Updates

---
+++ precache-manifest.860a5ebe54e0e5484463051c22f2b1a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880fb46acd7fc4c4e5e38562e61ebc34",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "dddb3e4ae80ea64d4b6b",
+    "url": "/my-portfolio/static/css/main.59bfad80.chunk.css"
+  },
+  {
+    "revision": "74dea5edf17e0ba8289f",
+    "url": "/my-portfolio/static/js/2.fe333af2.chunk.js"
+  },
+  {
+    "revision": "dddb3e4ae80ea64d4b6b",
+    "url": "/my-portfolio/static/js/main.f879b1ea.chunk.js"
+  },
+  {
+    "revision": "a97890d6b89f40023546",
+    "url": "/my-portfolio/static/js/runtime-main.027a5922.js"
+  }
+]);
