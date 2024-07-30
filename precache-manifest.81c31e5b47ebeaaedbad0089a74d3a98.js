bb3ce362cfa69a6b38c334e32115b93f6e7c6032
Prashant Bharti <46451926+prashantiitv@users.noreply.github.com>
2024-07-30 15:05:50 +0000

    Updates

--- precache-manifest.81c31e5b47ebeaaedbad0089a74d3a98.js
+++ precache-manifest.81c31e5b47ebeaaedbad0089a74d3a98.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b4eb390b46103f86de6eb1108ebcb01f",
+    "revision": "45e4c652f9e65795c037d3e9d0a110e4",
     "url": "/my-portfolio/index.html"
   },
   {
-    "revision": "59a0a55fc146cce4457e",
-    "url": "/my-portfolio/static/css/main.59bfad80.chunk.css"
+    "revision": "dfc01e3a3c6d96e278de",
+    "url": "/my-portfolio/static/css/main.0ef5d8b3.chunk.css"
   },
   {
     "revision": "74dea5edf17e0ba8289f",
     "url": "/my-portfolio/static/js/2.fe333af2.chunk.js"
   },
   {
-    "revision": "59a0a55fc146cce4457e",
-    "url": "/my-portfolio/static/js/main.9f723023.chunk.js"
+    "revision": "dfc01e3a3c6d96e278de",
+    "url": "/my-portfolio/static/js/main.41a60ee7.chunk.js"
   },
   {
     "revision": "a97890d6b89f40023546",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.9b025ece800d7f89e58fa757b399ae3d.js"
+  "/my-portfolio/precache-manifest.81c31e5b47ebeaaedbad0089a74d3a98.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
